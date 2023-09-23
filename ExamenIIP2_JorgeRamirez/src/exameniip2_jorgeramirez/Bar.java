746a152a76e208ddf1d4fa4848cfad875f864e79
Gael2019 <ramiritokillu@gmail.com>
2023-09-22 20:13:56 -0600

    commitf

--- ExamenIIP2_JorgeRamirez/src/exameniip2_jorgeramirez/Bar.java
+++ ExamenIIP2_JorgeRamirez/src/exameniip2_jorgeramirez/Bar.java
@@ -4,10 +4,26 @@
  */
 package exameniip2_jorgeramirez;
 
+import javax.swing.JProgressBar;
+
 /**
  *
  * @author Jorge Ramirez
  */
 public class Bar {
     
+    JProgressBar barra;
+    
+    //crear el hilo
+    public void run() {
+     
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hilo en ejecución: " + i);
+            try {
+                Thread.sleep(1000); // Duerme el hilo 
+            } catch (InterruptedException e) {
+             
+            }
+        }
+    }
 }
